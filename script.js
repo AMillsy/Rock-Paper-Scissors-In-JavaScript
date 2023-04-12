3d14668e350029a302fe862a192b033416c2a640
AMillsy <46033976+AMillsy@users.noreply.github.com>
2023-04-12 12:44:29 +0100

    Adding score and a response to wrong inputs

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const options = [`rock`, `paper`, `scissors`];
-
 let scores = [0, 0, 0]; //Players, Computers, Draws
-
 const forceResponse = `r`;
 
 function initializeGame() {
@@ -12,20 +10,25 @@ function initializeGame() {
   const computersChoice = Math.floor(Math.random() * 3); //Gives a number between
 
   const computerResponse = options[computersChoice].slice(0, 1).toLowerCase(); //Getting the first letter so if they type rock, or R or r the response will be valid
+  compareResponses(playerResponse, forceResponse);
 }
+
 function checkWinner(str) {
   if (forceResponse === str) {
+    scores[1]++;
     return alert(`Computer Wins!`);
   } else {
+    scores[0]++;
     return alert(`Player wins!`);
   }
 }
 
-function compareResponses() {
-  if (forceResponse === playerResponse) {
+function compareResponses(player, computer) {
+  if (computer === player) {
     alert(`You have drawn the match`);
+    scores[2]++;
   } else {
-    switch (playerResponse) {
+    switch (player) {
       case `r`:
         checkWinner(`p`);
         break;
@@ -35,6 +38,13 @@ function compareResponses() {
       case `s`:
         checkWinner(`r`);
         break;
+      default:
+        alert(`Incorrect Reponse, try again`);
     }
   }
+  initializeGame();
 }
+
+initializeGame();
+
+console.log(scores);
