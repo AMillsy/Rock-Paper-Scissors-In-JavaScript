12eecaa332eac6be56d9cd2b7772b16f4f1cf661
AMillsy <46033976+AMillsy@users.noreply.github.com>
2023-04-12 12:36:27 +0100

    Having response to each condition

--- script.js
+++ script.js
@@ -10,6 +10,31 @@ const computersChoice = Math.floor(Math.random() * 3); //Gives a number between
 
 const computerResponse = options[computersChoice].slice(0, 1).toLowerCase(); //Getting the first letter so if they type rock, or R or r the response will be valid
 
+const forceResponse = `r`;
 console.log(computerResponse);
 console.log(playerResponse);
 console.log(computersChoice);
+
+function checkWinner(str) {
+  if (forceResponse === str) {
+    return prompt(`Computer Wins!`);
+  } else {
+    return prompt(`Player wins!`);
+  }
+}
+
+if (forceResponse === playerResponse) {
+  alert(`You have drawn the match`);
+} else {
+  switch (playerResponse) {
+    case `r`:
+      checkWinner(`p`);
+      break;
+    case `p`:
+      checkWinner(`s`);
+      break;
+    case `s`:
+      checkWinner(`r`);
+      break;
+  }
+}
