32f3590e486ca7d56203b9b9c9db17c7b0e1aac8
AMillsy <46033976+AMillsy@users.noreply.github.com>
2023-04-12 12:14:23 +0100

    Adding a prompt for the user to pick

---
+++ script.js
@@ -0,0 +1,7 @@
+const options = [`rock`, `paper`, `scissors`];
+
+let scores = [0, 0, 0]; //Players, Computers, Draws
+
+const response = prompt(`Rock, Paper or Scissors?`);
+
+console.log(response);
