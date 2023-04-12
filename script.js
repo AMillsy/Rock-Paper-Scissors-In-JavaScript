27ebb62c79e4dea064255ec3940baddcbbaca976
AMillsy <46033976+AMillsy@users.noreply.github.com>
2023-04-12 12:19:38 +0100

    Getting a random number from 0 - 2

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ let scores = [0, 0, 0]; //Players, Computers, Draws
 const response = prompt(`Rock, Paper or Scissors?`);
 
 console.log(response);
+
+for (let i = 0; i < 100; i++) console.log(Math.floor(Math.random() * 3));
+
+const computersChoice = Math.floor(Math.random() * 3); //Gives a number between
+
+console.log(computersChoice);
