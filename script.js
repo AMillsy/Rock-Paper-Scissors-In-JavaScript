361a9a1fe6d7183eb27683adb907b664c74fb49b
AMillsy <46033976+AMillsy@users.noreply.github.com>
2023-04-12 12:25:20 +0100

    Making the response work in multiple ways

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const options = [`rock`, `paper`, `scissors`];
 
 let scores = [0, 0, 0]; //Players, Computers, Draws
 
-const response = prompt(`Rock, Paper or Scissors?`);
-
-console.log(response);
-
-for (let i = 0; i < 100; i++) console.log(Math.floor(Math.random() * 3));
+const playerResponse = prompt(`Rock, Paper or Scissors?`)
+  .slice(0, 1)
+  .toLowerCase(); //Gets the response and gets the first letter because R, P, S are all different letters and then make it lowercase.
 
 const computersChoice = Math.floor(Math.random() * 3); //Gives a number between
 
+const computerResponse = options[computersChoice].slice(0, 1).toLowerCase(); //Getting the first letter so if they type rock, or R or r the response will be valid
+
+console.log(computerResponse);
+console.log(playerResponse);
 console.log(computersChoice);
